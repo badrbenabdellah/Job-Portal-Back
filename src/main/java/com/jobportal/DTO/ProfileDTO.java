35d6c4d434dffca4e3e119aa867cba8eebf0c18d
badrbenabdellah <badr.benabdellah@etu.uae.ac.ma>
2025-04-04 15:19:17 +0000

    Filter

--- src/main/java/com/jobportal/DTO/ProfileDTO.java
+++ src/main/java/com/jobportal/DTO/ProfileDTO.java
@@ -21,6 +21,7 @@ public class ProfileDTO {
     private String location;
     private String about;
     private String picture;
+    private Long totalExp;
     private List<String> skills;
     private List<Experience> experiences;
     private List<Certification> certifications;
@@ -31,6 +32,6 @@ public class ProfileDTO {
     public Profile toEntity() {
         return new Profile( this.id, this.name, this.email, this.jobTitle, this.company, this.location,
                 this.about, this.picture != null ? Base64.getDecoder().decode(this.picture) : null,
-                this.skills, this.experiences, this.certifications, this.savedJobs);
+                this.totalExp, this.skills, this.experiences, this.certifications, this.savedJobs);
     }
 }
